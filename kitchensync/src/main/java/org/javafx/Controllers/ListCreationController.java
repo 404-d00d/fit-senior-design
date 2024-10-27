4bba41abe4c8ca90a162ff9ef3d0ff5a24fc9b09
404-d00d <raddivant@gmail.com>
2024-10-26 23:45:45 -0400

    created class to manage adding/removing items from shopping list

---
+++ kitchensync/src/main/java/org/javafx/Controllers/ListCreationController.java
@@ -0,0 +1,24 @@
+package org.javafx.Controllers;
+
+import org.javafx.Main.Main;
+
+import javafx.fxml.FXML;
+
+public class ListCreationController {
+    
+    // Method to create list for ingredients/cooking equipment
+    @FXML
+    private void addItemToShoppingList(){
+        // search bar that adds items to list
+        // run check if item exists
+        //      if item exists add it to database
+        //      then add item into ui and add a button to the side that lets you remove it
+        //      if not do not enter it
+    }
+
+    @FXML
+    private void removeItemFromShoppingList(){
+        // when button on side is clicked, should remove item from list and UI.
+    }
+
+}
